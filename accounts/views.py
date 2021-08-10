770a20572ccdd39acb4ca5ab3b1966fd1711353e
OceanLorDy <yunus0emr2@hotmail.com>
2021-08-10 16:04:41 +0300

    update Login & Requiring /have login error

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('articles:list')
+            user = form.save()
+            login(request, user)
+            return render(request, 'articles/article_list.html', {})
+
     else:
-        form = UserCreationForm()
+        form = AuthenticationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm()
+        form = AuthenticationForm(data=request.POST)
+        user = form.get_user()
+        login(request, user)
+        return render(request, 'articles/article_list.html', {})
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return render(request, 'articles/article_list.html', {})
--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.article_list, name="article"),
+    path("create/", views.article_create, name="create"),
     path("<slug>/", views.article_detail, name="detail"),
+
 ]
