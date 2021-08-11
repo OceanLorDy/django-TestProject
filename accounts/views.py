cc6a2feeebadb27c7510d2ba5744ddbe4092c404
OceanLorDy <yunus0emr2@hotmail.com>
2021-08-11 10:55:59 +0300

    update Login & Requiring

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 def signup_view(request):
@@ -11,19 +11,20 @@ def signup_view(request):
             login(request, user)
             return render(request, 'articles/article_list.html', {})
 
-    else:
-        form = AuthenticationForm()
+    form = AuthenticationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        user = form.get_user()
-        login(request, user)
-        return render(request, 'articles/article_list.html', {})
-    else:
-        form = AuthenticationForm()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("article")
+
+    form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
 
 
