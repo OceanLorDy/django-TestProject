3440a93031ceda3b99827b39ce08a5ea6f1c4765
OceanLorDy <yunus0emr2@hotmail.com>
2021-08-19 13:39:52 +0300

    update not working auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
 
 # Create your models here.
 
 class Account(models.Model):
-    name=models.CharField(max_length=30)
-    password=models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
 
     def __str__(self):
         return self.text + " " + self.password
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
